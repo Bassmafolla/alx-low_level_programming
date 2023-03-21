d842c5c2231385f74a41963e17cd7139a696be07
IBTISSAM <125500673+Bassmafolla@users.noreply.github.com>
2023-03-21 01:35:06 +0000

    hello

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - prints the alphabet in lowercase,
+* followed by a new line
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+putchar(65);
+putchar('\n');
+return (0);
+}
