0b7bea42717e4c8e90fb85ffcdcb81ffbc5020e3
IBTISSAM <125500673+Bassmafolla@users.noreply.github.com>
2023-04-07 00:40:11 +0000

    puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+# inclure  " main.h "
+/* *
+* _puts_recursion - imprime une chaîne
+* @s:chaîne
+* Retour : nul
+ */
+void  _puts_recursion ( char *s)
+{
+si (*s)
+{
+	_putchar (*s);
+	_puts_recursion (s + 1 );
+}
+autre
+{
+	_putchar ( ' \n ' );
+}
+
+}
