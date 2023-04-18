e348c2a93cc106f43493132911cc37d190795ee1
IBTISSAM <125500673+Bassmafolla@users.noreply.github.com>
2023-04-18 03:23:37 +0000

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,14 +10,15 @@ struct dog
 {
 char *name;
 float age;
-char *owner ;
+char *owner;
 };
-/* *
+/**
 * dog_t - Typedef for struct dog
+*
 */
-typedef  struct dog dog_t ;
-void  init_dog ( struct dog *d, char *name, float age, char *owner);
-void  print_dog ( struct dog *d);
-dog_t * new_dog ( char *nom, float age , char *owner);
-void  free_dog ( dog_t *d);
+typedef struct dog dog_t;
+void init_dog (struct dog *d, char *name, float age, char *owner);
+void print_dog (struct dog *d);
+dog_t *new_dog (char *nom, float age, char *owner);
+void free_dog (dog_t *d);
 # endif
