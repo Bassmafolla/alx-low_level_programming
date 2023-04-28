e6c387c9b5e4eae4a75db74ad1f7ebf00034700e
IBTISSAM <125500673+Bassmafolla@users.noreply.github.com>
2023-04-28 01:19:50 +0000

    stingly

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,28 +1,22 @@
-# inclure  " listes.h "
-# inclure  < stdio.h >
-
-/* *
-* print_list - Imprime tous les éléments d'une liste list_t.
-* @h : La liste list_t.
+#include <stdio.h>
+#include "listes.h"
+/**
+* print_list - imprime tous les éléments d'une liste chaînée
+* @h : pointeur vers la liste list_t à imprimer
 *
-* Retour : Le nombre de nœuds dans h.
- */
-
-size_t  print_list ( const  list_t *h)
+* Retour : le nombre de nœuds imprimés
+*/
+size_t print_list(const list_t *h)
 {
-	taille_t nœuds = 0 ;
-
-	tandis que (h)
-	{
-		si (h-> str == NULL )
-			printf ( " [0] (nul) \n " );
-
-		autre
-			printf ( " [ %d ] %s \n " , h-> len , h-> str );
-
-		nœuds++ ;
-		h = h-> suivant ;
-	}
-
-	retour (nœuds);
+taille_t s = 0 ;
+tandis que (h)
+{
+si (!h->chaîne)
+printf("[0] (néant)\n");
+autre
+printf("[%u] %s\n", h->len, h->str);
+h = h->suivant ;
+s++ ;
+}
+Retour);
 }
